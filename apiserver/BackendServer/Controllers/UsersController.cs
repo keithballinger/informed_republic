4817eb60de4fe2b86590aea95df818b20b7076cb
MikeStall <MikeStall@hotmail.com>
2013-06-22 14:55:59 -0700

    Get information from cloud storage instead of hard coding it.

--- apiserver/BackendServer/Controllers/UsersController.cs
+++ apiserver/BackendServer/Controllers/UsersController.cs
@@ -10,6 +10,9 @@ namespace BackendServer.Controllers
     // Describes an issue
     public class IssueState
     {
+        // Name of the issue
+        public string Name { get; set; }
+
         // How "hot" is the issue, from 1..100. 
         public int Hotness { get; set; }
     }
@@ -40,6 +43,8 @@ namespace BackendServer.Controllers
 
     public class UsersController : ApiController
     {
+        DataModel _model = new DataModel();
+
         // Returns all issues
         [HttpGet]
         public string[] GetIssues()
@@ -59,64 +64,52 @@ namespace BackendServer.Controllers
         [HttpGet]
         public string[] GetUserIssues(string user)
         {
-            return new string[] { "Privacy" };
+            return new string[] { "Privacy", "Immigration", "Healthcare" };
         }
 
         [HttpGet]
         public IssueState GetStatus(string issue)
         {
-            return new IssueState
+            // Immigration - 75 
+            // Privacy - 50 
+            // Healthcare - 10
+            return new IssueState            
             {
-                Hotness = 75
+                Name = issue,
+                Hotness = _model.GetRating(issue)
             };
+        }
+                        
+        [HttpGet]
+        public IssueState[] GetIssuesForUser(string user)
+        {
+            var issues = GetUserIssues(user);
+            var states = Array.ConvertAll(issues, issue => GetStatus(issue));
+            return states;
         }
 
         [HttpGet]
         public Alert[] GetAlerts(string user)
         {
-            return new Alert[]
-            {
-                new Alert
-                {
-                     Link = @"http://news.cnet.com/8301-1009_3-57588253-83/what-is-the-nsas-prism-program-faq/",
-                     Text = "Learn about PRISM",
-                     UrlImage = @"http://ts1.mm.bing.net/th?id=H.4872010103062804&pid=1.7&w=221&h=164&c=7&rs=1"
-                },
-                new Alert
-                {
-                    Link= @"http://www.bbc.co.uk/news/magazine-22853432",
-                    Text = "BBC on Prism",
-                    UrlImage = @"http://ts1.mm.bing.net/th?id=H.4679732980810272&pid=1.7&w=229&h=164&c=7&rs=1"
-                }
-            };
+            var issues = GetUserIssues(user);
+            var x = _model.GetAlerts(issues);
+            return x.ToArray();
         }
         
         [HttpGet]
         public Alert[] GetAction(string user)
         {
-            return new Alert[]
-            {
-                new Alert
-                {
-                    Text = "Sign petition to save Snowden",
-                    UrlImage = @"http://ts1.mm.bing.net/th?id=HB.210774609816&pid=1.7&w=242&h=164&c=7&rs=1"
-                }
-            };
+            var issues = GetUserIssues(user);
+            var x = _model.GetActions(issues);
+            return x.ToArray();
         }
-
-
+        
         [HttpGet]
         public TimelineData[] GetTimeline(string user, DateTime start, DateTime end)
         {
-            return new TimelineData[]
-            {
-                new TimelineData
-                {
-                     Date = new DateTime(2013, 6,1),
-                     Text  = "Snowden charged!",
-                     Link = @"http://news.msn.com/crime-justice/us-charges-snowden-with-espionage?ocid=ansnews11"
-                }
-            };
+            var issues = GetUserIssues(user);
+            var x = _model.GetEvents(issues);
+            return x.ToArray();
         }
 
         [HttpGet]
@@ -126,11 +119,35 @@ namespace BackendServer.Controllers
             { 
                 new Player
                 {
+                    Name= "Mark Zuckerberg",
+                    Link="http://en.wikipedia.org/wiki/Mark_Zuckerberg",
+                    Text="Assessment: His FWD.us group has been on the airwaves but the impact they are having is still nebulous. Will the group up their ad buys to help push wavering Senators into supporting the comprehensive bill? Still unclear...",
+                    UrlImage="http://ts2.mm.bing.net/th?id=H.4620204779047973&pid=1.7&w=168&h=173&c=7&rs=1"
+                },
+
+                new Player
+                {
+                    Name="Ted Cruz (R-TX)",
+                    Link="http://en.wikipedia.org/wiki/Ted_Cruz",
+                    Text="Assessment: In his six months in the Senate, he has shown a willingness to take on all 99 Senators. Will he use his power as a single Senator to place roadblocks on the progress of comprehensive reform?",
+                    UrlImage="http://ts4.mm.bing.net/th?id=H.4893819929299603&pid=1.7&w=222&h=176&c=7&rs=1"
+                },
+
+                new Player
+                {
+                    Name="John McCain (R-AZ)",
+                    Link="http://en.wikipedia.org/wiki/John_McCain",
+                    Text="Assessment: McCain maintains enormous power on the issue given his Arizona constituency and respect he has earned from both parties. His membership in the Gang of 8 and star power can help tilt this outcome over the coming week.",
+                    UrlImage="http://ts4.mm.bing.net/th?id=H.4524259476047939&pid=1.7&w=176&h=180&c=7&rs=1"
+                }
+                /*
+                new Player
+                {
                     Name = "Eric Snowden",
                     UrlImage = @"http://ts1.mm.bing.net/th?id=HB.210774609816&pid=1.7&w=242&h=164&c=7&rs=1",
                     Link = @"http://en.wikipedia.org/wiki/Edward_Snowden",
                     Text = "something about snowden"
-                }
+                }*/
             };
         }
     }
---
+++ apiserver/BackendServer/DataModel.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Auth;
+using Microsoft.WindowsAzure.Storage.Blob;
+using Newtonsoft.Json;
+
+namespace BackendServer.Controllers
+{
+    public partial class DataModel
+    {
+        private CloudBlobContainer Container()
+        {
+            CloudStorageAccount c = new CloudStorageAccount(
+                new StorageCredentials(_accountName, _accountKey), useHttps: false);
+
+            var client = c.CreateCloudBlobClient();
+            return client.GetContainerReference("data");
+        }
+
+        public int GetRating(string issue)
+        {
+            string name = string.Format("ratings/{0}.txt", issue.ToLower());
+            var c = Container();
+            var blob = c.GetBlobReferenceFromServer(name);
+            string val = Download(blob).Trim();
+
+            return int.Parse(val);
+        }
+
+        public IEnumerable<Alert> GetAlerts(IEnumerable<string> issues)
+        {
+            foreach (var issue in issues)
+            {
+                foreach (var x in GetAlerts(issue))
+                {
+                    yield return x;
+                }
+            }
+        }
+
+        // data/alerts/{issue}/{file}
+        public IEnumerable<Alert> GetAlerts(string issue)
+        {
+            string prefix = string.Format("alerts/{0}", issue.ToLower());
+            return GetList<Alert>(prefix);
+        }
+
+        public IEnumerable<Alert> GetActions(IEnumerable<string> issues)
+        {
+            foreach (var issue in issues)
+            {
+                foreach (var x in GetAction(issue))
+                {
+                    yield return x;
+                }
+            }
+        }
+
+
+        public IEnumerable<Alert> GetAction(string issue)
+        {
+            string prefix = string.Format("actions/{0}", issue.ToLower());
+            return GetList<Alert>(prefix);
+        }
+
+        public IEnumerable<TimelineData> GetEvents(IEnumerable<string> issues)
+        {
+            foreach (var issue in issues)
+            {
+                foreach (var x in GetEvents(issue))
+                {
+                    yield return x;
+                }
+            }
+        }
+
+
+        public IEnumerable<TimelineData> GetEvents(string issue)
+        {
+            string prefix = string.Format("events/{0}", issue.ToLower());
+            return GetList<TimelineData>(prefix);
+        }
+
+        public IEnumerable<T> GetList<T>(string prefix)
+        {
+            CloudBlobContainer c = Container();
+            foreach (var blob in c.ListBlobs(prefix: prefix, useFlatBlobListing: true))
+            {
+                string val = Download((ICloudBlob)blob);
+                T a = JsonConvert.DeserializeObject<T>(val);
+                yield return a;
+            }
+
+            yield break;
+        }
+
+        static string Download(ICloudBlob blob)
+        {
+            var b = ((CloudBlockBlob)blob);
+
+            MemoryStream ms = new MemoryStream();
+            b.DownloadToStream(ms);
+            b.FetchAttributes();
+            var props = b.Properties;
+
+            string result = System.Text.Encoding.UTF8.GetString(ms.ToArray());
+            return result;
+        }
+    }
+}
