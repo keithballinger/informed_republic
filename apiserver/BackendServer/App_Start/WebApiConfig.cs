479b6720453621389f02ced9e1bf645bccd577d2
MikeStall <MikeStall@hotmail.com>
2013-06-22 09:59:11 -0700

    Initial add for backend API server

---
+++ apiserver/BackendServer/App_Start/WebApiConfig.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+
+namespace BackendServer
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+
+            // Add RPC-style API
+            config.Routes.MapHttpRoute(
+                name: "hack2013",
+                routeTemplate: "api/{controller}/{action}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+
+#if false
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+#endif
+
+        }
+    }
+}
